12f7140a1aa557e266e146e499f6b7f01b2fe325
Elliot Harrison <elliotharr@live.co.uk>
2017-08-29 20:44:56 +0100

    Update Space-Time coding-analysis.m

--- Space-Time coding-analysis.m
+++ Space-Time coding-analysis.m
@@ -1,33 +1,37 @@
 clear all;
 snrdb = [0 :5 :20 ];
+
+% Iterate through the signal to noise data ratio matrix.
 for k = 1 : length(snrdb)
     SNR=10^(snrdb(k)/10);
     ct=100;
 
     sum1=0;
     sum2=0;
+
+% Create upperbound of data analysis iterations and initialize two random data modulation ranges.
     for i = 1 : ct
-        %transmiter
+        % Transmitter.
         s1 = randsrc(1, 1, [3 1 -1 -3]);
         s2 = randsrc(1, 1, [3 1 -1 -3]);
-        h1 = randn(1,1);
+        h1 = randn(1,1); % Create two channels for data and timecode transfer.
         h2 = randn(1,1);
-        n1 = randn(1,1)/sqrt(SNR);
-        n2 = randn(1,1)/sqrt(SNR);
+        n1 = randn(1,1)/sqrt(SNR); % Create first time transfer noise.
+        n2 = randn(1,1)/sqrt(SNR); % Create noise on transfer of time code.
 
-        % observation
-        y1 = h1 * s1 + h2 * s2 + n1; %first time
-        y2 = h1 * s2 + h2 * (-s1) + n2; %first time
-        %y3 = h1^2.1 * s + h2^2.1 * s + n;
-
-        %combine
+        % Observation.
+        y1 = h1 * s1 + h2 * s2 + n1; % First time without time code
+        y2 = h1 * s2 + h2 * (-s1) + n2; % First time with time code
+       
+        % Combine the two datasets.
         xs1 = h1*y1 - h2*y2;
         xs2 = h2*y1+ h1*y2;
-        %a rough estimation
+        
+        % Rough estimation.
         prio_est1 = xs1/(h1^2+h2^2);%first step of detection 
         prio_est2 =  xs2/(h1^2+h2^2);
         
-        %refine our estimation 
+        % Refine our estimation against sub modulative bands.
          if prio_est1 > 2
             est1= 3;
         elseif prio_est1 < 2 && prio_est1 > 0
@@ -52,7 +56,7 @@ for k = 1 : length(snrdb)
             est2 = 1;
         end 
         
-        %we want to know the errors we made
+        % We want to know the errors we made
         if est1 ~= s1
             sum1 = sum1+1;
         end
@@ -64,4 +68,4 @@ for k = 1 : length(snrdb)
     p1(k)= sum1/ct;
     p2(k) = sum2/ct;
 end
-semilogy(snrdb,p1,snrdb,p2)
+semilogy(snrdb,p1,snrdb,p2) % Plot transfer lifetime on semiology
