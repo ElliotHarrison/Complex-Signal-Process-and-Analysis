42b0df888becbbdd8b2f55adb39c547103fb890a
Elliot Harrison <elliotharr@live.co.uk>
2017-08-29 20:35:27 +0100

    Update Single-antenna-analysis.m

--- Single-antenna-analysis.m
+++ Single-antenna-analysis.m
@@ -1,25 +1,31 @@
 clear all;
 snrdb = [0  10  20  30];
+
+% Iterate through the signal to noise data ratio matrix.
 for k = 1 : length(snrdb)
     SNR=10^(snrdb(k)/10);
     ct=10000;
 
     sum1=0;
     sum2=0;
+    
+% Create upperbound of data analysis iterations and initialize random data modulation band.    
     for i = 1 : ct
         s = randsrc(1, 1, [-1 1]);
-        h1 = randn(1,1);
+        h1 = randn(1,1); % Create one transmit and receive channel (antenna) for data passthrough.
         h2 = randn(1,1);
-        n = randn(1,1)/sqrt(SNR);
-
+        n = randn(1,1)/sqrt(SNR); % Generate random AWGN noise.
+     
+% Receive data observations on signal plus noise.
         y1 = h1 * s+ h2 * s + n;
         y2 = abs(h1) * s + abs(h2) * s + n;
-        %y3 = h1^2.1 * s + h2^2.1 * s + n;
-
-        prio_est1 =y1/(h1+h2);%first step of detection 
+     
+% Detect data using prior estimates of observation output.
+        prio_est1 =y1/(h1+h2); % first step of detection 
         prio_est2 = y2/(abs(h1)  + abs(h2));
         
-        est1 = sign(prio_est1); %second step
+% Refine the prior estimates, with a conditional check of datasum condition from input to output.         
+        est1 = sign(prio_est1); % second step
         est2 = sign(prio_est2);
 
         if est1 ~= s
@@ -33,4 +39,4 @@ for k = 1 : length(snrdb)
     p1(k)= sum1/ct;
     p2(k) = sum2/ct;
 end
-semilogy(snrdb,p1,snrdb,p2)
+semilogy(snrdb,p1,snrdb,p2) % Plot transfer lifetime on semiology
